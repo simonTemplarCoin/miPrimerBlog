98dc16908647bfeb6278f4adb80e6cc8a6f54472
simonTemplarCoin <msb.coin@gmail.com>
2024-05-01 18:59:13 +0200

    first commit666789

--- django_24/build.sh
+++ django_24/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Exit on error
 set -o errexit
 
@@ -9,4 +9,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python manage.py migrate 
+python manage.py migrate
